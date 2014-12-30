729fde4e30918126c8ba2b81e940d3fd104cb884
Taylor <tharless7@gmail.com>
2014-12-30 16:20:45 -0500

    Wrote the code for exercise 17

--- ex17.py
+++ ex17.py
@@ -3,15 +3,7 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
-
 indata = open(from_file).read()
-
-print "The input file is %d bytes long" % len(indata)
-
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
@@ -19,3 +11,5 @@ out_file.write(indata)
 print "Alright, all done."
 
 out_file.close()
+
+# 1: done
