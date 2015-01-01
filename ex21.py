f4aae29fcac36e9c8cc7a33fa79856ff6a4e27f2
Taylor <tharless7@gmail.com>
2015-01-01 13:33:57 -0500

    Fixed bugs on ex21.py

--- ex21.py
+++ ex21.py
@@ -1,5 +1,5 @@
 def add(a, b):
-    print "ADDING %d + %D" % (a, b)
+    print "ADDING %d + %d" % (a, b)
     return a + b
 
 def subtract(a, b):
@@ -10,7 +10,7 @@ def multiply(a, b):
     print "MULTIPLYING %d * %d" % (a, b)
     return a * b
 
-def divide(a, b);
+def divide(a, b):
     print "DIVIDING %d / %d" % (a, b)
     return a / b
 
@@ -22,12 +22,12 @@ height = subtract(78, 4)
 weight = multiply(90, 2)
 iq = divide(100, 2)
 
-print "Age: %d\n Height: %d\n IQ: %d" % (age, height, iq)
+print "Age: %d\nHeight: %d\nIQ: %d" % (age, height, iq)
 
 
 
 # A puzzle for extra credit, type it in anyway.
-print "Here is a puzzle."
+# print "Here is a puzzle."
 
-what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
-print "That becomes: ", what\n, "Can you do it by hand?"
+# what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+# print "That becomes: ", what\n, "Can you do it by hand?"
