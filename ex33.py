f54680e84731dcd74c29ff2075d610d4ae282956
Taylor <tharless7@gmail.com>
2015-01-05 17:14:30 -0500

    Revised ex33.py

--- ex33.py
+++ ex33.py
@@ -1,20 +1,16 @@
 numbers = []
 
 def iterator(num, inc):
-    i = 0
-    while i < num:
+    for i in range(0, int(num), int(inc)):
         print "At the top i is %d" % i
         numbers.append(i)
-        i += int(inc)
         print "Numbers now: " , numbers
         print "At the bottom i is %d" % i
-    else:
-        break
 
-numero = raw_input("How high would you like to iterate?  ")
+num = raw_input("How high would you like to iterate?  ")
 inc = raw_input("By what increment would you like to iterate?  ")
 
-iterator(numero, inc)
+iterator(num, inc)
 
 print "The numbers: "
 
