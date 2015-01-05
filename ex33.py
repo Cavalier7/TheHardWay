ed93cd12f9efad12d570e734c054b9cede17074c
Taylor <tharless7@gmail.com>
2015-01-05 16:58:11 -0500

    Finished study drill #1 for ex33.py

--- ex33.py
+++ ex33.py
@@ -1,16 +1,21 @@
 i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % d
-    numbers.append(i)
+def iterator(num):
+    i = 0
+    while i < num:
+        print "At the top i is %d" % i
+        numbers.append(i)
     
-    i = i + 1
-    print "Numbers now: " , numbers
-    print "At the bottom i is %d" % i
+        i += 1
+        print "Numbers now: " , numbers
+        print "At the bottom i is %d" % i
 
+iterator(6)
 
 print "The numbers: "
 
 for num in numbers:
     print num
+
+# 1: done
