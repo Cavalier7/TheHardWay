9faf6c096c50c225198dc669bb4433f3a130d959
Taylor <tharless7@gmail.com>
2015-01-08 15:05:55 -0500

    Added basic check for valid moves

--- connect.py
+++ connect.py
@@ -10,7 +10,7 @@ board = []
 # create the board
 def create_board():
     for each in range(height):
-        board.append(width * [space],)
+        board.append(width * [space])
 
 # format the board to print
 def print_board():
@@ -23,4 +23,29 @@ print board
 
 print_board()
 
+# check to see if input is an integer 1 through 7
+
+#def check_input(move):
+    # try:
+        
+
+# check to see if a move is a valid integer move
+def check_board(move):
+    for row in range(height):
+        try: 
+            board[row][int(move)-1] == '0'
+            return True
+        except:
+            pass
+
+print check_board(0)
+print check_board(10)
+print check_board('a')
+print check_board(True)
+
 # prompt user for a move
+#while True:
+    #print "Welcome to Connect Four!  You get to move first - which column would you like?"
+    #move = raw_input('> ')
+    #if Column:
+        
